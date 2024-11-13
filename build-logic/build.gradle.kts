2785a488d5bb7103e70759cb510ca408f40362a3
Javier Segovia Córdoba <javier@segoviacordoba.com>
2024-11-13 14:25:03 +0100

    Add Kotlin Gradle plugin to build-logic

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -10,3 +10,16 @@ gradlePlugin {
         }
     }
 }
+
+dependencies {
+    implementation(libs.plugins.kotlin.jvm.artifact)
+}
+
+val Provider<PluginDependency>.artifact: Provider<ExternalModuleDependency>
+    get() = map {
+        dependencies.create(
+            group = it.pluginId,
+            name = "${it.pluginId}.gradle.plugin",
+            version = it.version.displayName,
+        )
+    }
