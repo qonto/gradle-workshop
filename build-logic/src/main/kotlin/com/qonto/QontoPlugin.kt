76921952f591106d5459d470c29459fdcc11b7b4
Javier Segovia Córdoba <javier@segoviacordoba.com>
2024-11-13 13:25:41 +0100

    Create Qonto plugin

---
+++ build-logic/src/main/kotlin/com/qonto/QontoPlugin.kt
@@ -0,0 +1,11 @@
+package com.qonto
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class QontoPlugin : Plugin<Project> {
+
+    override fun apply(target: Project) {
+        target.logger.quiet("Hello from QontoPlugin!")
+    }
+}
