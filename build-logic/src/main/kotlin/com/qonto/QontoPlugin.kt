da2472c87666492fc03f14d3506fc568edef7e68
Javier Segovia Córdoba <javier@segoviacordoba.com>
2024-11-13 14:29:06 +0100

    Add generated dir to the main Kotlin source set (wrong way)

--- build-logic/src/main/kotlin/com/qonto/QontoPlugin.kt
+++ build-logic/src/main/kotlin/com/qonto/QontoPlugin.kt
@@ -4,12 +4,23 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.plugins.BasePlugin
 import org.gradle.kotlin.dsl.apply
+import org.gradle.kotlin.dsl.configure
+import org.jetbrains.kotlin.gradle.dsl.KotlinProjectExtension
 
 class QontoPlugin : Plugin<Project> {
 
     override fun apply(target: Project) {
         target.pluginManager.apply(BasePlugin::class)
         target.logger.quiet("Hello from QontoPlugin!")
+
         QontoGenerateProjectDataTask.register(target)
+
+        target.pluginManager.withPlugin("org.jetbrains.kotlin.jvm") {
+            target.configure<KotlinProjectExtension> {
+                sourceSets.named("main") {
+                    kotlin.srcDirs(target.layout.buildDirectory.dir("generated/kotlin"))
+                }
+            }
+        }
     }
 }
