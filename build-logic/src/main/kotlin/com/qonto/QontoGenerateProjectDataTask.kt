1c456f24d461eeb042840de07b834afd4fee77ed
Javier Segovia Córdoba <javier@segoviacordoba.com>
2024-11-13 13:36:23 +0100

    Add Gradle task depends on assemble task

--- build-logic/src/main/kotlin/com/qonto/QontoGenerateProjectDataTask.kt
+++ build-logic/src/main/kotlin/com/qonto/QontoGenerateProjectDataTask.kt
@@ -4,6 +4,7 @@ import javax.inject.Inject
 import org.gradle.api.DefaultTask
 import org.gradle.api.Project
 import org.gradle.api.logging.Logger
+import org.gradle.api.plugins.BasePlugin
 import org.gradle.api.tasks.TaskAction
 import org.gradle.api.tasks.TaskProvider
 import org.gradle.kotlin.dsl.register
@@ -34,6 +35,9 @@ open class QontoGenerateProjectDataTask
                     name = NAME,
                     LoggerFactory.getLogger("qonto"),
                 )
+            project.tasks.named(BasePlugin.ASSEMBLE_TASK_NAME).configure {
+                dependsOn(generateProjectData)
+            }
         }
     }
 }
