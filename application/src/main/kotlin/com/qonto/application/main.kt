64084578d03f5e3a795394dbdc84644bae99b072
Javier Segovia Córdoba <javier@segoviacordoba.com>
2024-11-13 14:47:08 +0100

    Add the Gradle task outputs to the Kotlin main source set

--- application/src/main/kotlin/com/qonto/application/main.kt
+++ application/src/main/kotlin/com/qonto/application/main.kt
@@ -1,5 +1,12 @@
 package com.qonto.application
 
 fun main() {
-    println("Hello, world!")
+    println(
+        """
+            Project data:
+            Group: ${com.qonto.Project.group}
+            Name: ${com.qonto.Project.name}
+            Version: ${com.qonto.Project.version}
+        """.trimIndent()
+    )
 }
