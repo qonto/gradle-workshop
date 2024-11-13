2c8f86fc5d2770fe91e793d004e00855bf62ec2f
Javier Segovia Córdoba <javier@segoviacordoba.com>
2024-11-13 13:35:36 +0100

    Apply the Gradle Base plugin

--- build-logic/src/main/kotlin/com/qonto/QontoPlugin.kt
+++ build-logic/src/main/kotlin/com/qonto/QontoPlugin.kt
@@ -2,10 +2,13 @@ package com.qonto
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.plugins.BasePlugin
+import org.gradle.kotlin.dsl.apply
 
 class QontoPlugin : Plugin<Project> {
 
     override fun apply(target: Project) {
+        target.pluginManager.apply(BasePlugin::class)
         target.logger.quiet("Hello from QontoPlugin!")
         QontoGenerateProjectDataTask.register(target)
     }
